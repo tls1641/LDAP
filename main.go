bf94ef841e8ff7074fb8d7a68075bb3636a9bd02
shjhwoo <shjhwoo@trustnhope.com>
2022-09-13 16:56:12 +0900

    service modify/ delete ing

--- main.go
+++ main.go
@@ -2,11 +2,10 @@ package main
 
 import (
 	"fmt"
-	"html/template"
 	"log"
 	"net/http"
 	"project/main/ldapServer"
-	"project/main/users"
+	"project/main/ldapServer/Services"
 )
 
 // type Reqinfo struct {
@@ -41,70 +40,76 @@ func main() {
 	}
 	fmt.Println(l)
 
-	http.HandleFunc("/", userHandler)
+	// http.HandleFunc("/", userHandler)
+
+	// Services.CreateNewService("medic-app")
+	// Services.AddServiceHospitalMember("c00040","medic-app")
+	Services.RemoveServiceHostpitalMember("c00040","medic-app")
 	http.ListenAndServe("", nil)
 }
 
-func userHandler(w http.ResponseWriter, r *http.Request) {
+// func userHandler(w http.ResponseWriter, r *http.Request) {
 
-	switch r.URL.Path {
-	case "/sign-in":
-		signInUser(w, r)
-	case "/sign-up":
-		signUpUser(w, r)
-	case "/sign-in-form":
-		getSignInPage(w, r)
-	case "/sign-up-form":
-		getSignUpPage(w, r)
-	}
-}
+// 	switch r.URL.Path {
+// 	case "/sign-in":
+// 		signInUser(w, r)
+// 	case "/sign-up":
+// 		signUpUser(w, r)
+// 	case "/sign-in-form":
+// 		getSignInPage(w, r)
+// 	case "/sign-up-form":
+// 		getSignUpPage(w, r)
+// 	}
+// }
 
-func signInUser(w http.ResponseWriter, r *http.Request) {
-	newUser := getUser(r)
-	ok := users.DefaultUserService.VerifyUser(newUser)
-	if !ok {
-		fileName := "sign-in.html"
-		t, _ := template.ParseFiles(fileName)
-		t.ExecuteTemplate(w, fileName, "User Sign-in Failure")
-		return
-	}
-	fileName := "sign-in.html"
-	t, _ := template.ParseFiles(fileName)
-	t.ExecuteTemplate(w, fileName, "User Sign-in Success")
-	return
-}
+// func signInUser(w http.ResponseWriter, r *http.Request) {
+// 	newUser := getUser(r)
+// 	ok := users.DefaultUserService.VerifyUser(newUser)
+// 	if !ok {
+// 		fileName := "sign-in.html"
+// 		t, _ := template.ParseFiles(fileName)
+// 		t.ExecuteTemplate(w, fileName, "User Sign-in Failure")
+// 		return
+// 	}
+// 	fmt.Println("로그인 성공")
+// 	fileName := "sign-in.html"
+// 	t, _ := template.ParseFiles(fileName)
+// 	t.ExecuteTemplate(w, fileName, "User Sign-in Success")
+// 	return
+// }
 
-func signUpUser(w http.ResponseWriter, r *http.Request) {
-	newUser := getUser(r)
-	err := users.DefaultUserService.CreateUser(newUser)
-	if err != nil {
-		fileName := "sign-up.html"
-		t, _ := template.ParseFiles(fileName)
-		t.ExecuteTemplate(w, fileName, "New User Sign-up Failure")
-	}
-	fileName := "sign-up.html"
-	t, _ := template.ParseFiles(fileName)
-	t.ExecuteTemplate(w, fileName, "New User Sign-up Success")
-}
+// func signUpUser(w http.ResponseWriter, r *http.Request) {
+// 	newUser := getUser(r)
+// 	err := users.DefaultUserService.CreateUser(newUser)
+// 	if err != nil {
+// 		fileName := "sign-up.html"
+// 		t, _ := template.ParseFiles(fileName)
+// 		t.ExecuteTemplate(w, fileName, "New User Sign-up Failure")
+// 	}
+// 	fmt.Println("회원가입 성공")
+// 	fileName := "sign-up.html"
+// 	t, _ := template.ParseFiles(fileName)
+// 	t.ExecuteTemplate(w, fileName, "New User Sign-up Success")
+// }
 
-func getUser(r *http.Request) users.User {
-	email := r.FormValue("email")
-	password := r.FormValue("password")
-	return users.User{
-		Email:    email,
-		Password: password,
-	}
-}
+// func getUser(r *http.Request) users.User {
+// 	email := r.FormValue("email")
+// 	password := r.FormValue("password")
+// 	return users.User{
+// 		Email:    email,
+// 		Password: password,
+// 	}
+// }
 
-func getSignInPage(w http.ResponseWriter, r *http.Request) {
-	templating(w, "sign-in.html", nil)
-}
+// func getSignInPage(w http.ResponseWriter, r *http.Request) {
+// 	templating(w, "sign-in.html", nil)
+// }
 
-func getSignUpPage(w http.ResponseWriter, r *http.Request) {
-	templating(w, "sign-up.html", nil)
-}
+// func getSignUpPage(w http.ResponseWriter, r *http.Request) {
+// 	templating(w, "sign-up.html", nil)
+// }
 
-func templating(w http.ResponseWriter, fileName string, data interface{}) {
-	t, _ := template.ParseFiles(fileName)
-	t.ExecuteTemplate(w, fileName, data)
-}
+// func templating(w http.ResponseWriter, fileName string, data interface{}) {
+// 	t, _ := template.ParseFiles(fileName)
+// 	t.ExecuteTemplate(w, fileName, data)
+// }
